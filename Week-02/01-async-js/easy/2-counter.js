b4234b96a379f55df04571a3fc17c88dc450a6e7
Vaibhav Chitransh <vaibhavchitranshbdn@gmail.com>
2026-09-18 16:49:35 +0530

    counter using setTimeout

--- Week-02/01-async-js/easy/2-counter.js
+++ Week-02/01-async-js/easy/2-counter.js
@@ -3,3 +3,26 @@
 // Without using setInterval, try to code a counter in Javascript. There is a hint at the bottom of the file if you get stuck.
 
 // (Hint: setTimeout)
+
+// Method 1
+let prev = Date.now();
+let i = 1;
+
+while(true) {
+    let curr = Date.now();
+    if(curr - prev == 1000) {
+        console.log(i++);
+        prev = curr;
+    }
+}
+
+
+// Method 2
+let count = 0;
+
+function startCounter() {
+  console.log(count++);
+  setTimeout(startCounter, 1000);
+}
+
+startCounter();
