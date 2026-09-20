06cd4b32ca89ad99c941f1ab8583091793cdbd43
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-09-20 19:34:58 +0530

    read from file

--- Week-02/01-async-js/easy/3-read-from-file.js
+++ Week-02/01-async-js/easy/3-read-from-file.js
@@ -5,3 +5,22 @@
 // Try to do an expensive operation below the file read and see how it affects the output. 
 // Make the expensive operation more and more expensive and see how it affects the output. 
 
+const fs = require('fs');
+
+const startTime = Date.now();
+
+// I/O bound task (async function)
+fs.readFile("sample.txt", "utf-8", function (err, data) {
+    if(err) {
+        console.log("Error reading file");
+    } else {
+        const currTime = Date.now();
+        console.log("Printing after " + (currTime - startTime)/1000 + " sec");
+        console.log(data);
+    }
+})
+
+// an expensive task (cpu-bound)
+for(let i=0; i<10000000000; i++) {
+
+}
