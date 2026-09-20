1555782c41d270053fadf8f606fc5fc33978ee64
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-09-20 19:46:49 +0530

    file cleaning (removing extra spaces)

--- Week-02/01-async-js/medium/1-file-cleaner.js
+++ Week-02/01-async-js/medium/1-file-cleaner.js
@@ -11,3 +11,16 @@
 // ```
 // hello world my name is raman
 // ```
+
+const fs = require('fs');
+
+fs.readFile("sample.txt", "utf-8", function (err, data) {
+    if(err) {
+        console.log("Error while reading the file");
+    } else {
+        let text = data.replaceAll(/\s+/g, ' ').trim();
+        fs.writeFile("sample.txt", text, function () {
+            console.log("Done cleaning the file");
+        }) 
+    }
+})
