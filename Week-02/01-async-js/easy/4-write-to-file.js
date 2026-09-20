b0e76b9c74071b8e3f8b7bc9377838d0725f98fd
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-09-20 19:38:43 +0530

    write to file using fs

--- Week-02/01-async-js/easy/4-write-to-file.js
+++ Week-02/01-async-js/easy/4-write-to-file.js
@@ -2,3 +2,11 @@
 
 // Using the fs library again, try to write to the contents of a file.
 // You can use the fs library to as a black box, the goal is to understand async tasks.
+
+const fs = require('fs');
+
+const newContent = "This is the new content of the file.";
+
+fs.writeFile("sample.txt", newContent, function () {
+    console.log("New content is written in the file");
+})
